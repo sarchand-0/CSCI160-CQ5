be232c3ba25a9d782719a58529d96094961a6ab9
sarchand-0 <65009344+sarchand-0@users.noreply.github.com>
2023-10-18 13:01:05 -0400

    Add files via upload

---
+++ Mars.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'marsExploration' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int marsExploration(String s) {
+        String expectedSOS = "SOS";
+        int changes = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != expectedSOS.charAt(i % 3)) {
+                changes++;
+            }
+        }
+
+        return changes;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.marsExploration(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
